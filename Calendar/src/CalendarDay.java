7cb5df3b656fe737647075281bf9ae93c731936e
Stephen Fulton <s639f423@ku.edu>
2016-09-08 21:14:42 -0500

    Created Calendar project, moved class files over

---
+++ Calendar/src/CalendarDay.java
@@ -0,0 +1,44 @@
+//EECS 448 Project 1
+//Stephen Fulton, Shawn Parkes, and Rebekah Manweiler
+//Java CalendarDay class
+
+public class CalendarDay
+{
+	private int date = 0;
+	private String month;
+	private String dayOfWeek;
+	private String[] dayEvents;
+	
+	public CalendarDay(int d, String m, String dow)
+	{
+		date = d;
+		month = m;
+		dayOfWeek = dow;
+		dayEvents = new String[0];
+	}
+	
+	public int getDate()
+	{
+		return date;
+	}
+	
+	public String getMonth()
+	{
+		return month;
+	}
+	
+	public String getDayOfWeek()
+	{
+		return dayOfWeek;
+	}
+	
+	public String getEvents()
+	{
+		String temp = "";
+		for (int i = 0; i < dayEvents.length; i++)
+		{
+			temp += i + ". " + dayEvents[i] + "\n";
+		}
+		return temp;
+	}
+}
---
+++ Calendar/src/CalendarDriver.java
@@ -0,0 +1,10 @@
+
+public class CalendarDriver {
+	
+	public static void main(String[] args)
+	{
+		CalendarDay x = new CalendarDay(5,"September","Sunday");
+		System.out.println(x.getEvents());
+	}
+
+}
