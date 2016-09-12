0f4e00cbfebe0f725be805528f1ace002d95b747
Stephen Fulton <s639f423@ku.edu>
2016-09-12 14:13:16 -0500

    Updated CalendarDriver class

--- Calendar/src/CalendarDriver.java
+++ Calendar/src/CalendarDriver.java
@@ -4,13 +4,7 @@ public class CalendarDriver {
 	
 	public static void main(String[] args)
 	{
-		CalendarDay x = new CalendarDay(5,"September");
-		x.addEvent("this sucks a lot");
-		x.addEvent("this, like, really sucks");
-		System.out.println(x.getEvents());
-		x.removeEvent(1);
-		System.out.println(x.getEvents());
-		//CalendarJFrame y = new CalendarJFrame();
+		
 	}
 
 }
